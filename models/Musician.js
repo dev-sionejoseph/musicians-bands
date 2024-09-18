1ff4f6ae822ee77f41490d64c52f9d26c3968c21
Sione <dev.sionejoseph@gmail.com>
2024-09-18 14:17:03 -0500

    Musician model defined

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,16 @@
-const {Sequelize, sequelize} = require('../db');
+const {Sequelize, db, DataTypes, Model} = require('../db');
 
-// TODO - define the Musician model
-let Musician;
+// TODO - define the Band model
+class Musician extends Model {
+
+}
+
+Musician.init({
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING,
+    sequelize: db,
+    modelName: 'Musician'
+})
 
 module.exports = {
     Musician
